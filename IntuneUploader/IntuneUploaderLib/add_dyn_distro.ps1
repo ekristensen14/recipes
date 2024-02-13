3e2644d8491a36f5330ae1c6f03137a1e0ebf21c
Esben Rebacz <erebaczadmin@project44.com>
2024-02-13 07:47:02 +0100

    update code

---
+++ IntuneUploader/IntuneUploaderLib/add_dyn_distro.ps1
@@ -0,0 +1,23 @@
+$csv = Import-Csv "/Users/erebacz/distro_csv.csv" -Delimiter ";"
+
+foreach($row in $csv){
+    # Check if distro exists
+    $assigned = Get-DistributionGroup -Identity $row.assigned
+    if(!$assigned){
+        "Assigned group ($($row.assigned)) does not exist, stopping"
+        return
+    }
+    $dynamic = Get-DynamicDistributionGroup -Identity $row.dynamic
+    if(!$dynamic){
+        "Dynamic group ($($row.dynamic)) does not exist, stopping"
+        return
+    }
+    "Adding '$($row.dynamic)' to '$($row.assigned)'"
+    try {
+        Add-DistributionGroupMember -Identity $row.assigned -Member $row.dynamic
+        "Success"
+    }
+    catch {
+        "Failure"
+    }
+}
